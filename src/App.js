8776b069d181900830f1944e0c3ce8bdca6bdb5f
jacko529 <churchillj54@gmail.com>
2019-03-18 10:11:34 +0000

    pokemon added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React, { Component } from 'react';import './App.css';
+import React, { Component } from 'react';
+
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+
+import './App.css';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+
 import NavBar from "./Components/Layout/NavBar";
 import Dashboard from './Components/Layout/Dashboard';
+import Pokemon from './Components/pokemon/Pokemon';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavBar/>
-        <div className="container"><Dashboard/></div>
-      </div>
+      <Router>
+        <div className="App" >
+          <NavBar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
--- src/Components/pokemon/PokemonList.js
+++ src/Components/pokemon/PokemonList.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
-import PokemondCard from './PokemonCard';
+
+import PokemonCard from './PokemonCard';
+import Loading from '../Layout/Loading';
+import axios from 'axios';
 
 export default class PokemonList extends Component {
+  state = {
+    url: 'https://pokeapi.co/api/v2/pokemon/',
+    pokemon: null
+  };
+
+  async componentDidMount() {
+    const res = await axios.get(this.state.url);
+    this.setState({ pokemon: res.data['results'] });
+  }
+
   render() {
     return (
-      <div className="row">
-        <PokemondCard/>
+      <div>
+        {this.state.pokemon ? (
+          <div className="row">
+            {this.state.pokemon.map(pokemon => (
+              <PokemonCard
+                key={pokemon.name}
+                name={pokemon.name}
+                url={pokemon.url}
+              />
+            ))}
+          </div>
+        ) : (
+          <Loading />
+        )}
       </div>
-    )
+    );
   }
 }
